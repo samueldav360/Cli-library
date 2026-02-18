c76895a439009132ffb0a6874e56cd714a459c43
samueldav360 <samueldav360@gmail.com>
2026-02-18 07:42:26 -0500

    Re

--- LibraryCLI/biblioteca/CLI.py
+++ LibraryCLI/biblioteca/CLI.py
@@ -22,14 +22,14 @@ def run_cli():
 
         try:
 
-            # -------------------------
+          
             if option == "1":
                 title = input("Title: ")
                 author = input("Author: ")
                 b = service.add_book(title, author)
                 print(f"Book added with id {b.id}")
 
-            # -------------------------
+        
             elif option == "2":
                 books = service.list_books()
                 if not books:
@@ -38,26 +38,26 @@ def run_cli():
                     status = "LOANED" if b.loaned_to else "AVAILABLE"
                     print(f"{b.id} | {b.title} | {b.author} | {status}")
 
-            # -------------------------
+            
             elif option == "3":
                 name = input("User name: ")
                 u = service.register_user(name)
                 print(f"User registered with id {u.id}")
 
-            # -------------------------
+           
             elif option == "4":
                 book_id = int(input("Book id: "))
                 user_id = int(input("User id: "))
                 service.loan_book(book_id, user_id)
                 print("Loan created")
 
-            # -------------------------
+ 
             elif option == "5":
                 book_id = int(input("Book id: "))
                 service.return_book(book_id)
                 print("Book returned")
 
-            # -------------------------
+            
             elif option == "6":
                 loans = service.list_active_loans()
                 if not loans:
@@ -65,7 +65,7 @@ def run_cli():
                 for l in loans:
                     print(f"Book {l.book_id} → User {l.user_id}")
 
-            # -------------------------
+          
             elif option == "0":
                 print("Bye")
                 break
@@ -73,9 +73,7 @@ def run_cli():
             else:
                 print("Invalid option")
 
-        # -------------------------
-        # manejo de excepciones del dominio
-        # -------------------------
+        
 
         except BookNotFound:
             print("Error: Book not found")
